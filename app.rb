b8b64b469602d687bad91c1280e03b805eb5ee86
isabella3412 <izzybella17@icloud.com>
2018-06-26 15:56:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
+get '/name' do
+  "My name is Isabella"
+end
+
+get '/hometown' do
+  "My hometown is Linthicum Heights"
+end
+
+get '/favorite-song'
+  "My favorite song is Born Bob Dylan by the Veronicas"
+end
 class App < Sinatra::Base
 end
